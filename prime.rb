c565c4f7db8e72b958f1a54400912132b9378016
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-18 02:46:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+   if integer <= 1
+     is_disvible = ((integer % 2) == 0) 
+      if is_disvible
+      return false
+     end
+   end 
+   true
+ end
