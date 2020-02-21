f7a1bd6e0de5056ca8a9417846b38fbcd7b1ff04
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-21 01:03:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,15 @@
 require 'pry'
-def prime?(integer)
-n = 2
-  while n < integer
-    return false if integer % n < 0
-    n += 1
-  end
-  true
-end
-
 # def prime?(integer)
-# if integer / 2
-#   false
-# end 
+# n = 2
+#   while n < integer
+#     return false if integer % n < 0
+#     n += 1
+#   end
+#   true
 # end
+
+def prime?(integer)
+if integer / 2 == 0
+  false
+end 
+end
