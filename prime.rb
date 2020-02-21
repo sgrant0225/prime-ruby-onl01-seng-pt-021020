2a80e66106e8e3c1fbd4673202baf255e3e51d5c
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-21 00:58:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,17 @@
 require 'pry'
 
+# def prime?(integer)
+#   if integer <= 1
+#     is_disvible = ((integer % 2) == 0) 
+#       if is_disvible
+#       return false
+#     end
+#   end 
+#   true
+# end
+
 def prime?(integer)
-   if integer <= 1
-     is_disvible = ((integer % 2) == 0) 
-      if is_disvible
-      return false
-     end
-   end 
-   true
- end
+ if integer / 2
+   false
+ end 
+end
